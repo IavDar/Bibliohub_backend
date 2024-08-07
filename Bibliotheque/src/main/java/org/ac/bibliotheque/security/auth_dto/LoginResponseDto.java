0f92e62e11b157547826deae672d16497b40cc1c
Vladn09 <nozhik09@gmail.com>
2024-08-07 19:08:29 +0200

    fr

--- Bibliotheque/src/main/java/org/ac/bibliotheque/security/auth_dto/LoginResponseDto.java
+++ Bibliotheque/src/main/java/org/ac/bibliotheque/security/auth_dto/LoginResponseDto.java
@@ -3,13 +3,18 @@ package org.ac.bibliotheque.security.auth_dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.ac.bibliotheque.role.Role;
+
+import java.util.Set;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginResponseDto {
+    private Long id;
     private String accessToken;
     private String refreshToken;
     private String message;
+    private Set<Role> role;
 }
 
