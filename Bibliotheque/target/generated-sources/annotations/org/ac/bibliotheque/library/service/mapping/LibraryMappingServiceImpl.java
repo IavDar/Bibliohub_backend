66342748a3e6f75153947be8e2d6a560e8d97973
Vladn09 <nozhik09@gmail.com>
2024-08-09 16:59:02 +0200

    util

--- Bibliotheque/target/generated-sources/annotations/org/ac/bibliotheque/library/service/mapping/LibraryMappingServiceImpl.java
+++ Bibliotheque/target/generated-sources/annotations/org/ac/bibliotheque/library/service/mapping/LibraryMappingServiceImpl.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-08-08T19:51:24+0200",
-    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.11 (Amazon.com Inc.)"
+    date = "2024-08-08T21:29:55+0200",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.12 (Amazon.com Inc.)"
 )
 @Component
 public class LibraryMappingServiceImpl implements LibraryMappingService {
